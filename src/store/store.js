176771397c4b3aaf23d1087b10f7314fa4c6cede
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-09-14 18:47:19 -0600

    Created store and slices

--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,12 @@
-// The store of the application is going to
-// be build here.
+import { configureStore } from "@reduxjs/toolkit";
+import genreSlice from "../slices/genreSlice";
+import searchSlice from "../slices/searchSlice";
+
+const store = configureStore({
+  reducer: {
+    genre: genreSlice,
+    search: searchSlice
+  }
+});
+
+export default store;
